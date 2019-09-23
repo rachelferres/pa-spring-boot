553aba20e1d0f62cfba84cb657fdd14d64783792
rachelferres <55385504+rachelferres@users.noreply.github.com>
2019-09-23 15:57:17 +0100

    update on to Do

--- 06-01-TODO/src/main/java/uk/ac/belfastmet/TODO/controller/ToDoController.java
+++ 06-01-TODO/src/main/java/uk/ac/belfastmet/TODO/controller/ToDoController.java
@@ -14,40 +14,57 @@ import uk.ac.belfastmet.TODO.service.ToDoService;
 @Controller
 public class ToDoController {
 	Logger logger = LoggerFactory.getLogger(ToDoController.class);
+	private String welcomeMsg = "Welcome. Here is your to do list";
+	private char[] charMsg = {'a', 'b', 'c'};
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String homePage(Model model) {
 		
 		ToDoService toDoService = new ToDoService();
-		
+		welcomeMsg = "Welcome. Here is your to do list";
 		
 		model.addAttribute("toDo", toDoService.getToDoList());
-		
+		model.addAttribute("welcome", welcomeMsg);
 		return "index";
 		
 	}
 	
 	@RequestMapping(value="/completed", method = RequestMethod.GET)
 	public String completed(Model model) {
-		
+		welcomeMsg = "Get Cracking!!";
 		ToDoService toDoService = new ToDoService();
 		logger.info("HERE IS AN INFO WARNING");
 		
 		model.addAttribute("toDo", toDoService.getCompletedToDoList());
-		
+		model.addAttribute("welcome", welcomeMsg);
 		return "completed";
 		
 	}
 	
 	@RequestMapping(value="/incomplete", method = RequestMethod.GET)
 	public String incomplete(Model model) {
+		welcomeMsg = "Well Done!";
+
 		
 		ToDoService toDoService = new ToDoService();
 		logger.info("HERE IS AN INFO WARNING");
 		
 		model.addAttribute("toDo", toDoService.getInCompletedToDoList());
+		model.addAttribute("welcome", welcomeMsg);
 		
 		return "incomplete";
+		
+	}
+	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		
+		
+		welcomeMsg = "Welcome. Please log in using your username and password";
+		
+		
+		model.addAttribute("welcome", welcomeMsg);
+		return "login";
 		
 	}
 
